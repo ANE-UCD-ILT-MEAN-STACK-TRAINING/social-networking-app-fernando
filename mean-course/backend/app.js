326f6875f9ac2bd72eec7953c45adff2ea82744b
F.S.(Crosstown) <royalplanet2009@gmail.com>
2020-05-10 01:30:58 -0400

    Modifying backend get. Compledted section 12 (lab 3)

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -52,21 +52,27 @@ app.post("/api/post", (req, res, next) => {
 
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server",
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!",
-    },
-  ];
-  res.status(200).json({
-    message: "Posts fetched succesfully!",
-    posts: posts,
+  // const posts = [
+  //   {
+  //     id: "fadf12421l",
+  //     title: "First server-side post",
+  //     content: "This is coming from the server",
+  //   },
+  //   {
+  //     id: "ksajflaj132",
+  //     title: "Second server-side post",
+  //     content: "This is coming from the server!",
+  //   },
+  // ];
+  // res.status(200).json({
+  //   message: "Posts fetched succesfully!",
+  //   posts: posts,
+  // });
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: "Post fetched successfully",
+      post: documents,
+    });
   });
 });
 
